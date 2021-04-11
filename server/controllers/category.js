6100b2bd22d435d16c5e6ef1a5eb377b8f5941a8
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-11 18:58:11 -0300

    servicio para crear categoria implementado

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -2,15 +2,15 @@ const { request, response } = require('express');
 const category_service = require('../services/category');
 
 const create = async (req = request, res = response, next) => {
-  let code = 201;
-  let resContent = { ok: true };
-
   try {
-    await category_service.create(req.body);
+    const data = await category_service.create(req.body);
 
-    res.status(code).json(resContent);
+    res.status(201).json({
+      ok: true,
+      data,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
