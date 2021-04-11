ea8a8ad50556cfa177401e72595629eba60d3c5f
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-11 17:31:11 -0300

    errores añadidos

---
+++ server/errors/errors.js
@@ -0,0 +1,98 @@
+class BadRequest extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 400;
+    this.errorMessage = message || 'No se ha pedido bien el recurso';
+  }
+}
+class AlreadyExists extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 401;
+    this.errorMessage = message || 'El recurso ya existe';
+  }
+}
+class InvalidCredentials extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 401;
+    this.errorMessage = message || 'Credenciales invalidas';
+  }
+}
+class NotFound extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 404;
+    this.errorMessage = message || 'El recurso no se encuentra';
+  }
+}
+class RequireLogin extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 406;
+    this.errorMessage = message || 'El recurso necesita que ingreses al sitio';
+  }
+}
+class RequireToken extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 401;
+    this.errorMessage =
+      message || 'Debes enviar un token en la petición.';
+  }
+}
+class RequirePermission extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 401;
+    this.errorMessage =
+      message || 'No tienes los permisos para acceder al recurso';
+  }
+}
+class InternalServerError extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 500;
+    this.errorMessage = message || 'Servicio no disponible';
+  }
+}
+
+class IncompleteData extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 400;
+    this.errorMessage = message || 'Faltan datos';
+  }
+}
+
+class ExistingEmail extends Error {
+  constructor(message) {
+    super();
+    Error.captureStackTrace(this, this.constructor);
+    this.statusCode = 404;
+    this.errorMessage =
+      message || 'Ya se encuentra un usuario registrado con el mail enviado.';
+  }
+}
+
+module.exports = {
+  BadRequest,
+  AlreadyExists,
+  NotFound,
+  InternalServerError,
+  RequireLogin,
+  RequireToken,
+  RequirePermission,
+  InvalidCredentials,
+  IncompleteData,
+  ExistingEmail,
+};
