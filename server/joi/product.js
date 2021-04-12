3b3a8d752a6c6347d19d8e6a5ec2ee5c1b9d4f45
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-12 11:49:33 -0300

    ahora tambien se va a validar el campo  'categoryId' a la hora de crear un producto

--- server/joi/product.js
+++ server/joi/product.js
@@ -4,6 +4,7 @@ const create = Joi.object({
   name: Joi.string().required(),
   price: Joi.number().min(0).required(),
   stock: Joi.number().min(1).required(),
+  categoryId: Joi.number(),
 });
 
 module.exports = { create };
