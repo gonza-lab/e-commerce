f98d715766670eedea2bc2d97276b477b359be78
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-12 12:33:14 -0300

    ahora se puede recibir un valor 'null' en el campo 'categoryId', en la ruta para actualizar un producto

--- server/joi/product.js
+++ server/joi/product.js
@@ -15,7 +15,7 @@ const update = Joi.object({
   name: Joi.string(),
   price: Joi.number().min(0),
   stock: Joi.number().min(0),
-  categoryId: Joi.number(),
+  categoryId: [Joi.number(), null],
 }).or('name', 'price', 'stock', 'categoryId');
 
 module.exports = { id, create, update };
