90567c8a9819eb8ec98e78e8ed6e68617d446bc7
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-17 14:11:23 -0300

    ahora el modelo imagen tiene una nueva propiedad lllamada 'name'

--- server/migrations/20210412203657-create-image.js
+++ server/migrations/20210412203657-create-image.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+      name: {
+        type: Sequelize.STRING,
+      },
       url: {
         type: Sequelize.STRING,
       },
--- server/models/image.js
+++ server/models/image.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Image.init({
+    name: DataTypes.STRING,
     url: DataTypes.STRING,
     mime: DataTypes.STRING
   }, {
